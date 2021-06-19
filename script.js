ea1bec304188129da3e711dd9bcb4fd032cfa97e
Dorine van der Voorden <84196122+Dorinevandervoorden@users.noreply.github.com>
2021-06-19 12:32:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("Hallo Winc Academy Studenten");
+
+
+// Paint Walls
+const paintWall = function (wallside, color) {
+    console.log("The " + wallside + "wall has been painted " + color);
+};
+
+paintWall("north", "green");
+paintWall("south-east", "white/yellow")
+
+//paintWall(); --> Will say "The wall has been painted undefined"
+
+
+/* number 7-8
+const paintWall = function (color, wallside) {
+    console.log("The " + wallside + "wall has been painted " + color);
+};
+
+paintWall("north", "green");
+paintWall("south-east", "white/yellow")
+
+paintWall("green", "north");
+*/
